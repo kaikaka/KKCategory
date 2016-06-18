bd4b37dab6c8856dca9e4b91183285405f98e005
观剑不则声 <yoon1583@foxmail.com>
2016-06-18 23:50:23 +0800

    [ADD]
    添加UITextField 类别

---
+++ UIKit/UITextField/UITextField+NLLimit.h
@@ -0,0 +1,15 @@
+//
+//  UITextField+NLLimit.h
+//  iKuaiLao
+//
+//  Created by xiangkai yin on 16/6/3.
+//  Copyright © 2016年 kuailao_2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITextField (NLLimit)
+
+@property (assign, nonatomic) NSUInteger nl_maxLength;
+
+@end
